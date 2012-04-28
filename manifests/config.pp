3a75f8fdf791e7edf51bc4c993f0b47dea3b4f0f
Kristian Øllegaard <kristian@kristian.io>
2012-04-28 02:03:40 +0200

    typo

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,6 @@
 class puppetagent::config (
 	$puppet_conf_file = $puppetagent::params::puppet_conf_file,
-	$plugin_sync = $puppetagent::params::plugin_sync,
+	$plugin_sync = $puppetagent::params::plugin_sync
 	) inherits puppetagent::params {
 	file {$puppet_conf_file:
 		template => 'puppet.conf.erb'
